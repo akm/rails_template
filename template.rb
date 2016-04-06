1a93cf77831cc7fb58ef7eadfc95a556db7a3d2e
akm <akm2000@gmail.com>
2016-04-06 12:31:58 +0900

    Add gems

--- template.rb
+++ template.rb
@@ -1,8 +1,19 @@
+# http://guides.rubyonrails.org/rails_application_templates.html
+# https://github.com/morizyun/rails4_template/blob/master/app_template.rb
+# https://github.com/search?q=rails%20template&source=c
+
 require 'bundler'
 
 run 'mv README.rdoc README.md'
 
 append_file 'Gemfile', <<-EOS
+
+gem "twitter-bootstrap-rails"
+
+gem 'devise'
+gem "kaminari"
+
+gem 'rails_admin'
 
 group :development, :test do
   gem "rspec"
@@ -22,6 +33,8 @@ end
 group :development do
   gem "better_errors"
   gem 'binding_of_caller'
+
+  gem "schema_comments"
 end
 
 EOS
