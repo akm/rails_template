9ec3f1a3799518923ccf536be5daf7a7930aaf31
Takeshi AKIMA <akm2000@gmail.com>
2016-09-20 10:27:43 +0900

    Don't show bullet warning on views

--- template.rb
+++ template.rb
@@ -250,7 +250,7 @@ insert_into_file 'config/environments/development.rb', <<EOS, after: "  # config
 
   config.after_initialize do
     Bullet.enable = true
-    Bullet.alert = true
+    Bullet.alert = false
     Bullet.bullet_logger = true
     Bullet.console = true
     # Bullet.growl = true
